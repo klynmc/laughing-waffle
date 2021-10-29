5d4cedeb7bb51bd81b4472f3f62af5c38ef94ddf
Leia <87383466+klynmc@users.noreply.github.com>
2021-10-29 18:15:43 -0500

    add Heroku

---
+++ server.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const newNotes = req.body
+    notes.push(newNotes)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes)
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const deleteNote = notes.filter((rmvNote) => rmvNote.id != req.params.id)
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
+    res.json(deleteNote)
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+// RESOURCES 
+// https://youtu.be/Qo71smMMQBc
