adabf5f7946b7137e2860ae826da3293873b9be6
Leia <87383466+klynmc@users.noreply.github.com>
2021-11-10 17:48:28 -0600

    updated code to function

--- server.js
+++ server.js
@@ -10,31 +10,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('/db/db.json'))
+    const newNotes = req.body
+    notes.push(newNotes)
+    fs.writeFileSync('/db/db.json', JSON.stringify(notes))
+    res.json(notes)
+});
+
+/* app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const deleteNote = notes.filter((rmvNote) => rmvNote.id != req.params.id)
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
+    res.json(deleteNote)
+}); */
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
-
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'))
-});
-
-app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
-    const newNotes = req.body
-    notes.push(newNotes)
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
-    res.json(notes)
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
-    const deleteNote = notes.filter((rmvNote) => rmvNote.id != req.params.id)
-    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
-    res.json(deleteNote)
 });
 
 app.listen(PORT, () => {
