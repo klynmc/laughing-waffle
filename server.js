33fd976b55c4effd239c3bcf80b0518fdef918ce
Leia <87383466+klynmc@users.noreply.github.com>
2021-11-10 21:25:22 -0600

    rearranged code

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
-
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'))
 });
@@ -35,6 +27,14 @@ app.delete('/api/notes/:id', (req, res) => {
     const deleteNote = notes.filter((rmvNote) => rmvNote.id != req.params.id)
     fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
     res.json(deleteNote)
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.listen(PORT, () => {
