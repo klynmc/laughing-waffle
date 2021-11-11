bb8ded0a354e1ddb81326b9228b289e70aee8e71
Leia <87383466+klynmc@users.noreply.github.com>
2021-11-10 22:14:07 -0600

    edit code

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('/db/db.json'))
-    const newNotes = req.body
-    newNotes.id = randomUUID();
-    notes.push(newNotes)
+    newNote.id = randomUUID();
+    notes.push(newNote)
     fs.writeFileSync('/db/db.json', JSON.stringify(notes))
     res.json(notes)
 });
